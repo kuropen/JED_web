3f85d38ba3a58b3bba2ebf362601b730d403162a
Hirochika Yuda <hyuda@outlook.jp>
2021-09-07 00:39:47 +0900

    Fix wrong theme config

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,14 @@
-import { Box, Flex, Heading, Link, LinkBox, LinkOverlay, VisuallyHidden } from "@chakra-ui/react"
+import { Box, ChakraProvider, extendTheme, Flex, Heading, Link, LinkBox, LinkOverlay, VisuallyHidden } from "@chakra-ui/react"
 import { InfoOutlineIcon } from "@chakra-ui/icons";
 import { Helmet } from "react-helmet";
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import { graphql, useStaticQuery, Link as GatsbyLink } from "gatsby"
+import themeConfig from "../utilities/themeConfig";
 
 import "@fontsource/orbitron/700.css"
+import "@ibm/plex/css/ibm-plex-sans-jp.css"
+import "@ibm/plex/css/ibm-plex.css"
 
 interface LayoutProps {
     pageTitle?: string
@@ -29,49 +32,53 @@ query LayoutMeta {
         titleShown = `${props.pageTitle} - ${title}`
     }
 
+    const customTheme = extendTheme(themeConfig)
+
     return (
-        <Box as="main" maxW={{base: "container.sm", lg: "container.lg", xl: "container.xl"}} mx="auto" mt="2" mb="2">
-            {/* TODO: 更新通知実装したときは pb=36 入れる */}
-            <Helmet title={titleShown} htmlAttributes={{lang: 'ja'}}>
-            </Helmet>
-            <Flex
-                my="2"
-                pb="2"
-                borderBottom="2px"
-                borderBottomColor="gray"
-                as="header"
-                align="center"
-            >
-                <LinkBox as="nav" d={{base: "none", md: "block"}}>
-                    <StaticImage src="../images/elecwarn_icon.png" alt="From Electricity Warning Project" width={48} />
-                    <LinkOverlay as={GatsbyLink} to="/"><VisuallyHidden>トップページに戻る</VisuallyHidden></LinkOverlay>
-                </LinkBox>
-                <Heading
-                    as="h1"
-                    size="xl"
-                    fontFamily="Orbitron, sans-serif"
-                    textAlign="center"
-                    flex="1"
+        <ChakraProvider theme={customTheme}>
+            <Box as="main" maxW={{base: "container.sm", lg: "container.lg", xl: "container.xl"}} mx="auto" mt="2" mb="2">
+                {/* TODO: 更新通知実装したときは pb=36 入れる */}
+                <Helmet title={titleShown} htmlAttributes={{lang: 'ja'}}>
+                </Helmet>
+                <Flex
+                    my="2"
+                    pb="2"
+                    borderBottom="2px"
+                    borderBottomColor="gray"
+                    as="header"
+                    align="center"
                 >
-                    <Link as={GatsbyLink} to="/">
-                        Japan Electricity Dashboard
-                    </Link>
-                </Heading>
-                <LinkBox as="nav">
-                    <InfoOutlineIcon w="32px" h="32px" />
-                    <LinkOverlay as={GatsbyLink} to="/about"><VisuallyHidden>はじめに</VisuallyHidden></LinkOverlay>
-                </LinkBox>
-            </Flex>
-            <Box>
-                {children}
+                    <LinkBox as="nav" d={{base: "none", md: "block"}}>
+                        <StaticImage src="../images/elecwarn_icon.png" alt="From Electricity Warning Project" width={48} />
+                        <LinkOverlay as={GatsbyLink} to="/"><VisuallyHidden>トップページに戻る</VisuallyHidden></LinkOverlay>
+                    </LinkBox>
+                    <Heading
+                        as="h1"
+                        size="xl"
+                        fontFamily="Orbitron, sans-serif"
+                        textAlign="center"
+                        flex="1"
+                    >
+                        <Link as={GatsbyLink} to="/">
+                            Japan Electricity Dashboard
+                        </Link>
+                    </Heading>
+                    <LinkBox as="nav">
+                        <InfoOutlineIcon w="32px" h="32px" />
+                        <LinkOverlay as={GatsbyLink} to="/about"><VisuallyHidden>はじめに</VisuallyHidden></LinkOverlay>
+                    </LinkBox>
+                </Flex>
+                <Box>
+                    {children}
+                </Box>
+                <Box as="address" fontStyle="normal" mx="2" mt="5">
+                    Copyright &copy; <Link href="https://kuropen.org/" rel="author" isExternal>Kuropen</Link>. 
+                    Licensed under <Link href="https://creativecommons.org/licenses/by-nc-sa/4.0/" rel="license" isExternal>Creative Commons Attribution-NonCommercial-ShareAlike 4.0</Link> License.<br />
+                    Electricity Warning Project Icon is provided by <Link href="https://twitter.com/haretter" isExternal>Hareta</Link> and 
+                    licensed under <Link href="https://creativecommons.org/licenses/by-nc-sa/3.0/" isExternal>Creative Commons Attribution-NonCommercial-ShareAlike 3.0</Link> License.
+                </Box>
             </Box>
-            <Box as="address" fontStyle="normal" mx="2" mt="5">
-                Copyright &copy; <Link href="https://kuropen.org/" rel="author" isExternal>Kuropen</Link>. 
-                Licensed under <Link href="https://creativecommons.org/licenses/by-nc-sa/4.0/" rel="license" isExternal>Creative Commons Attribution-NonCommercial-ShareAlike 4.0</Link> License.<br />
-                Electricity Warning Project Icon is provided by <Link href="https://twitter.com/haretter" isExternal>Hareta</Link> and 
-                licensed under <Link href="https://creativecommons.org/licenses/by-nc-sa/3.0/" isExternal>Creative Commons Attribution-NonCommercial-ShareAlike 3.0</Link> License.
-            </Box>
-        </Box>
+        </ChakraProvider>
     )
 }
 
