6d43a4478f2290979841cae1db5ed071eceba115
Hirochika Yuda <hyuda@outlook.jp>
2021-09-11 21:21:41 +0900

    Remove SSR element

--- utilities/chakraColorModeManager.tsx
+++ utilities/chakraColorModeManager.tsx
@@ -24,15 +24,3 @@ export const ChakraProviderWithColorModeProps: React.FC<PropsWithChildren<Chakra
         <ChakraProvider {...props} colorModeManager={colorModeManager} theme={customTheme} />
     )
 }
-
-// also export a reusable function getServerSideProps
-export const getServerSideProps: Partial<GetServerSideProps<ChakraProviderWithColorModePropsElements>> = (params: {req: IncomingMessage}) => {
-    const req: IncomingMessage = params.req
-    return {
-        props: {
-            // first time users will not have any cookies and you may not return
-            // undefined here, hence ?? is necessary
-            cookies: req.headers.cookie ?? "",
-        },
-    }
-}
