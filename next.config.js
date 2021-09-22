d793004abaf33c2c9379f2d8e3b81743b16258c9
Hirochika Yuda <hyuda@outlook.jp>
2021-09-23 00:31:40 +0900

    Security Headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,32 @@
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  }
+]
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      }
+    ]
+  },
 }
