38e7b2924c1489b0263b4df7ee90e6ded5d20dc3
Hirochika Yuda <hyuda@outlook.jp>
2021-10-06 00:40:23 +0900

    Update HSTS settings

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const securityHeaders = [
   },
   {
     key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload'
+    value: 'max-age=31536000; includeSubDomains'
   }
 ]
 
